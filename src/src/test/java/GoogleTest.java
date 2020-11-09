7ec4c3f3940252fab3a97329be258a3fd2f25c0a
Sergei Pershin <sergei.pershin@live.com>
2020-11-09 23:40:40 +0200

    Added test for Chrome and Firefox Browsers. Used different selectors for Tests.

--- src/src/test/java/GoogleTest.java
+++ src/src/test/java/GoogleTest.java
@@ -1,16 +1,26 @@
-import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.text;
 
 public class GoogleTest {
 
     @Test
-    void selenideSearch() {
+    void selenideSearchInChrome() {
+        Configuration.browser = "chrome";
         open("http://google.com");
         $(byName("q")).setValue("Selenide").pressEnter();
-        $("html").shouldHave(Condition.text("Selenide"));
+        $("html").shouldHave(text("Selenide: concise UI tests in Java"));
+    }
+
+    @Test
+    void selenideSearchInFirefox() {
+        Configuration.browser = "firefox";
+        open("http://google.com");
+        $(byName("q")).setValue("Selenide").pressEnter();
+        $(byId("search")).shouldHave(text("Selenide: concise UI tests in Java"));
     }
 }
